f1d37b20d145fbb7a793c83ff2b0221f4b4af49c
Mirco Attocchi <amirco@gmail.com>
2018-05-27 11:52:50 +0200

    importato material lite

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'material-design-lite/material.min.css'
+import 'material-design-lite/material.min'
 
 Vue.config.productionTip = false
 
